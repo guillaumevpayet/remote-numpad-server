ad3c529c7111329323f3ce373b0c746c9bf8c1c9
Guillaume Payet <theolizard@gmail.com>
2021-04-13 12:04:42 +1200

    Changed the BlueCove dependency to a MavenCentral version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,18 +26,12 @@ version = "1.5.1"
 
 repositories {
     mavenCentral()
-
-    flatDir {
-        dirs("libs")
-    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.4.1")
-    implementation("net.sf.bluecove:bluecove:2.1.1-SNAPSHOT")
-//    implementation("net.sf.bluecove:bluecove-gpl:2.1.1-SNAPSHOT")
-//    implementation("net.sf.bluecove:bluecove-bluez:2.1.1-SNAPSHOT")
+    implementation("io.ultreia:bluecove:2.1.1")
 //    implementation("com.github.hypfvieh:dbus-java:3.2.3")
     implementation("org.scijava:native-lib-loader:2.3.4")
 }
